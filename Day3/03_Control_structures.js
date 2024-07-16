a78f6daa99d454798f0a093d36e6015d8f7d9d44
iamtinkal <116683248+iamtinkal@users.noreply.github.com>
2024-07-16 17:27:02 +0000

    day 3

---
+++ Day3/03_Control_structures.js
@@ -0,0 +1,51 @@
+//Activity 1 (if else, write a program to check if a number is  positive, negative or zero and log the result)
+
+// let num1= 2342342;
+
+
+//     if(num1==0){
+//         return console.log("Number is 0")
+//     }
+//     else if (num1 > 0){
+//         return console.log("Number is Positive")
+//     }
+//     else {
+//         return console.log("Number is Negative")
+//     }
+//     ;
+
+
+//Task2 : write a program if a person is eligible for a vote age >=18
+
+
+// age = 19
+// let conclusion = age >=18 ? "Eligible for Vote": "Not Eligible for Vote"
+// console.log(conclusion);
+
+
+//Activity 2
+
+// write a program to find largest of three numbers using nested else if statements
+
+number1 = 400
+number2 = 231
+number3 = 200
+
+
+if(number1>number2 && number1>number3){
+    console.log(number1, "is the largest")
+}
+else if(number2>number3 && number2>number1){
+    console.log(number2, "is the largest")
+}
+else{ console.log(number3, "is the largest")};
+
+
+
+
+
+
+
+
+
+
