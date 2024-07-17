05f87ebec89aeabba7eb6ed8b11b8bc0b6cd3aa3
iamtinkal <116683248+iamtinkal@users.noreply.github.com>
2024-07-17 16:56:58 +0000

    day 4

---
+++ Day4/04_loops.js
@@ -0,0 +1,46 @@
+//Activity 1
+//task1: write a program to print no 1 to 10 using loops
+
+// for(i=1; i<=10; i++){
+//     console.log(i)
+// };
+
+//task2: write a for loop to print the table of 5
+
+// for(j=1; j<=10; j++){
+//     console.log("5*", j, "=", j*5)
+// };
+
+//Activity 2(while loops)
+//task3: write a program to calculate the sum of number   from 1 to 10 using while loop
+
+// let k = 1
+// let sum = 0
+// while(k<=10){
+//     sum += k
+//     k++
+// };
+// console.log(sum);
+
+//task 4: write a program to print no from 10 to 1 using while loops
+
+// let l = 11
+// while(l>=1){
+//     l--
+// console.log(l)
+// };
+
+
+
+//Activity 3: Do while loop
+//task 5: write a program to print no from 1 to 5 using do while loop,
+
+// let m = 1
+
+// do {
+//     console.log(m)
+//     m++
+// }while (m <= 5)
+
+
+//task6: 
