4394a79582ba5ee776a02f264f4e229ee2e6253e
iamtinkal <116683248+iamtinkal@users.noreply.github.com>
2024-07-23 06:59:12 +0000

    day6

--- Day6/06_array.js
+++ Day6/06_array.js
@@ -12,7 +12,7 @@ console.log(array1[0]);
 //last elememt
 console.log(array1[array1.length - 1]);
 
-//Activity 2: Array Methods
+//Activity 2: Array Methods (Basic)
 
 //Task 3: use the push method to add a new number to the end of the array and log the updated array.
 
@@ -34,3 +34,25 @@ console.log(array1);
 
 array1.unshift(1)
 console.log(array1);
+
+
+
+
+//Activity 3: Array Methods (Intermediate)
+
+// Task7: Use the map method to create a new array, where each number is doubled and log
+
+//The map() method of Array instances creates a new array populated with the results of calling a provided function on every element in the calling array.
+
+
+console.log(array2= array1.map((x) => x * 2))
+
+
+//Task 8: Use the filter method to create a new array with only even numbers, log the array.
+
+//The filter() method of Array instances creates a shallow copy of a portion of a given array, filtered down to just the elements from the given array that pass the test implemented by the provided function.
+
+console.log(array1.filter((x)=> x%2==0));
+
+
+//Task 9: use the reduce method to calculate the sum of all numbers in arra and log the array.
