5ac52a0c5567438fd3e68314e1e84b5a88bf243f
iamtinkal <116683248+iamtinkal@users.noreply.github.com>
2024-07-29 10:58:23 +0000

    Day8

---
+++ Day8/08_ES6_Features.js
@@ -0,0 +1,95 @@
+//Activity 1: Template Literals
+
+
+//Template literals are literals delimited with backtick (`) characters, allowing for multi-line strings, string interpolation with embedded expressions, and special constructs called tagged templates.
+
+
+//Task1: Use template literals to create a string that includes variables for a person's name and age, and log the string to the console.
+
+
+const name = "Tinkal"
+const age  =  29
+
+const string = `My name is ${name} and I am ${age} years old.`
+
+console.log(string);
+
+
+
+//Task 2: create a multi line string using template literals and log it into the console.
+
+const education = "B.Com"
+const place = "Delhi"
+
+const multi_line_string = `My name is ${name}.
+My age is ${age}.
+My higher education in ${education}
+My place of leaving is in ${place}`
+
+console.log(multi_line_string);
+
+
+
+
+//Activity 2: Destructuring
+
+//The destructuring assignment syntax is a JavaScript expression that makes it possible to unpack values from arrays, or properties from objects, into distinct variables.
+
+
+//Task 3: Use array destructuring to extract the first and second elements from an array of the number and log the result in console.
+
+const array1 = [1,2,3,4,5,6]
+
+const[first, second] = array1
+
+console.log(first, second);
+
+
+//Task 4: Use Object destructuring to extract the title and author from a book object and log them in the console.
+
+const object1 = {
+    'title': 'Atmoic Habbit',
+    'Year': 2005,
+    'Author': 'Author'
+}
+
+const{title,Year,Author} = object1
+
+console.log(title,Year,Author);
+
+
+
+//Activity 3: Spread and Rest Operators
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
